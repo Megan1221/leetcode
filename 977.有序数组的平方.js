63f3bbbc269a0091e3fee5476fb888d225cdf6be
马英健 <704886043@qq.com>
2023-02-28 15:25:29 +0800

    977

---
+++ 977.有序数组的平方.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=javascript
+ *
+ * [977] 有序数组的平方
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let left = 0, right = nums.length - 1
+  let idx = right
+  const res = nums.concat([])
+  while (idx >= 0) {
+    let rightSquare = nums[right] * nums[right]
+    let leftSquare = nums[left] * nums[left]
+    if (rightSquare >= leftSquare) {
+      res[idx--] = rightSquare
+      right--
+    } else {
+      res[idx--] = leftSquare
+      left++
+    }
+  }
+  return res
+};

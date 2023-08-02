fea3cf65670ba04001808e5b8d0c52977f9288dc
马英健（明恬） <mayingjian.myj@alibaba-inc.com>
2023-08-02 19:42:47 +0800

    tow pointer:27

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -19,6 +19,20 @@ var removeElement = function(nums, val) {
   }
   return len
 };
+// var removeElement = function (nums, val) {
+//   let left = 0, right = nums.length - 1
+//   while (left <= right) {
+//     if (nums[left] == val) {
+//       while (nums[right] === val && right >= left) {
+//         right--
+//       }
+//       if (right > left)
+//         [nums[left], nums[right]] = [nums[right], nums[left]]
+//     }
+//     left++
+//   }
+//   return nums[right] === val ? right : right + 1
+// };
 // @lc code=end
 // 和26方法二的解题思路基本一致
 // 对于需要删除的元素，直接用其他需要保留的值覆盖，而不用多次删除
